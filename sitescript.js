dbe8921a7d1e205686966596820378335cb7e783
Joao Ricardo <jricardollanque@gmail.com>
2024-04-17 17:43:55 +0100

    Almost done

--- sitescript.js
+++ sitescript.js
@@ -1,6 +1,9 @@
 const stars1 = document.getElementById('stars1');
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+const button4 = document.getElementById('button4');
+const button5 = document.getElementById('button5');
 
 document.addEventListener('mousemove', moveShip);
 document.addEventListener('click', shoot);
@@ -10,16 +13,32 @@ stars1.addEventListener('animationiteration', () => {
 });
 
 button1.addEventListener('click', () => {
-    window.open('https://www.google.com/search?q=fuck+you+meaning&oq=fuck+you+meaning&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDMwMjRqMGoxqAIAsAIA&sourceid=chrome&ie=UTF-8', '_blank');
+    window.open('https://www.google.com/mars/', '_blank');
 });
 
 button2.addEventListener('click', () => {
 
-    window.open('https://www.google.com/search?q=ugly+meaning&sca_esv=d43f5111e59e559f&ei=aM4fZo-8OKenkdUP5vCroAM&udm=&ved=0ahUKEwiPkMKarcmFAxWnU6QEHWb4CjQQ4dUDCBA&uact=5&oq=ugly+meaning&gs_lp=Egxnd3Mtd2l6LXNlcnAiDHVnbHkgbWVhbmluZzIGEAAYBxgeMgYQABgHGB4yBhAAGAcYHjIGEAAYBxgeMgYQABgHGB4yBhAAGAcYHjIGEAAYBxgeMgYQABgHGB4yBhAAGAcYHjIIEAAYBxgeGA9I8wpQAFjfBHAAeACQAQCYAY4BoAHJA6oBAzEuM7gBA8gBAPgBAZgCBKACkwTCAggQABgHGB4YE8ICChAAGAcYHhgPGBOYAwCSBwMwLjSgB_8U&sclient=gws-wiz-serp', '_blank');
+    window.open('https://www.google.com/moon/', '_blank');
+});
+
+button3.addEventListener('click', () => {
+
+    window.open('https://www.google.com/search?q=do+a+barrel+roll&sca_esv=d43f5111e59e559f&ei=o-UfZtXgOOCG9u8P-_a8qAs&udm=&gs_ssp=eJzj4tVP1zc0TIo3SMmuMKoyYPQSSMlXSFRISiwqSs1RKMrPyQEApoAKcw&oq=do+a+barrel&gs_lp=Egxnd3Mtd2l6LXNlcnAiC2RvIGEgYmFycmVsKgIIADIKEC4YgAQYigUYQzIKEAAYgAQYigUYQzIFEAAYgAQyChAAGIAEGIoFGEMyBRAAGIAEMgUQABiABDIKEAAYgAQYigUYQzIKEAAYgAQYigUYQzIFEAAYgAQyChAAGIAEGIoFGEMyGRAuGIAEGIoFGEMYlwUY3AQY3gQY4ATYAQFIyiFQAFjcEnAAeACQAQCYAasBoAGZCqoBAzQuN7gBAcgBAPgBAZgCC6AC2QrCAgsQLhiABBjHARjRA8ICDhAuGK8BGMcBGIAEGI4FwgIOEC4YgAQYxwEYrwEYjgXCAgsQLhiABBjHARivAcICBRAuGIAEwgINEC4YgAQYxwEY0QMYCpgDALoGBggBEAEYFJIHAzMuOKAHw3M&sclient=gws-wiz-serp#ip=1', '_blank');
+});
+
+button4.addEventListener('click', () => {
+
+    window.open('https://www.google.com/search?q=personal+meaning&sca_esv=d43f5111e59e559f&ei=7-AfZrnIIeKH9u8P2sWryAk&udm=&ved=0ahUKEwj5-_zvvsmFAxXig_0HHdriCpkQ4dUDCBA&uact=5&oq=personal+meaning&gs_lp=Egxnd3Mtd2l6LXNlcnAiEHBlcnNvbmFsIG1lYW5pbmcyBhAAGAcYHjIGEAAYBxgeMgYQABgHGB4yBhAAGAcYHjIGEAAYBxgeMgYQABgHGB4yBhAAGAcYHjIGEAAYBxgeMgYQABgHGB4yBhAAGAcYHkjVDVAAWO8LcAB4AZABAJgBtgGgAbILqgEDMy45uAEDyAEA-AEBmAIGoAL0BMICChAAGIAEGIoFGEOYAwCSBwMyLjSgB7s-&sclient=gws-wiz-serp', '_blank');
+});
+
+button5.addEventListener('click', () => {
+
+    window.open('https://www.google.com/search?q=joke+meaning&sca_esv=d43f5111e59e559f&ei=GOEfZpyuBLOJ9u8P_umTuA8&ved=0ahUKEwicmqaDv8mFAxWzhP0HHf70BPcQ4dUDCBA&uact=5&oq=joke+meaning&gs_lp=Egxnd3Mtd2l6LXNlcnAiDGpva2UgbWVhbmluZzIFEAAYgAQyBhAAGBYYHjIGEAAYFhgeMgYQABgWGB4yCBAAGBYYHhgPMggQABgWGB4YDzIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHjIGEAAYFhgeSKG8KlDWE1j7GHAGeAGQAQCYAYYBoAGQBaoBAzIuNLgBA8gBAPgBAZgCCqACzQPCAgoQABhHGNYEGLADwgINEAAYgAQYigUYQxiwA8ICCRAAGIAEGA0YE8ICCBAAGAcYHhgTwgIGEAAYBxgemAMAiAYBkAYKkgcDOC4yoAeIIg&sclient=gws-wiz-serp', '_blank');
 });
 
 function moveShip(event) {
     const ship = document.getElementById('ship');
+
     ship.style.left = event.clientX - ship.offsetWidth / 2 + 'px';
     ship.style.top = event.clientY - ship.offsetHeight / 2 + 'px';
 }
@@ -55,6 +74,18 @@ function shoot(event) {
             clearInterval(interval);
             projectile.remove();
             button2.click();
+        } else if (collision(projectile, button3)) {
+            clearInterval(interval);
+            projectile.remove();
+            button3.click();
+        } else if (collision(projectile, button4)) {
+            clearInterval(interval);
+            projectile.remove();
+            button4.click();
+        } else if (collision(projectile, button5)) {
+            clearInterval(interval);
+            projectile.remove();
+            button5.click();
         }
     }, 10);
 }
